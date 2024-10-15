659a5dd470aec5bd11f650f55c55251abab6d25d
Frank Sena AGBOLOSU <francksena@yahoo.com>
2024-10-15 16:58:17 +0200

    script

--- code-projet/script.js
+++ code-projet/script.js
@@ -1,11 +1,18 @@
 // Sélectionne le bouton et le formulaire
-const wilders = document.getElementsByClassName('showForm');
+const wilders = document.querySelectorAll('.showForm');
 const form = document.getElementById('contactForm');
 // Ajoute un écouteur d'événement
-wilders.addEventListener('click', function() {
-    if(form.style.display === 'none' || form.style.display === ' '){
-        form.style.display = 'block';
-    } else{
-        form.style.display = 'none';
-    }
-} );
+for(const wilder of wilders){
+    wilder.addEventListener('click', function() {
+        if(form.style.display === 'none' || form.style.display === ' '){
+            form.style.display = 'block';
+        } else{
+            form.style.display = 'none';
+        }
+    } );
+}
+
+const submit = document.querySelector('#submit');
+submit.addEventListener('click', function () {
+    alert('Votre message a été envoyé');
+} )
